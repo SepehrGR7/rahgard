1d3dc5bed7249eed67b369cec8468c4bf2858ff8
Sepehr Gorgini <agent.1378@gmail.com>
2023-03-09 16:36:35 +0330

    Add hover effect

--- src/components/LinkButton.jsx
+++ src/components/LinkButton.jsx
@@ -2,7 +2,10 @@ import { Link } from 'react-router-dom'
 
 const LinkButton = ({ to, variant, size, weight, children }) => {
   return (
-    <Link to={to} className={`btn btn-${variant} btn-${size} font-${weight}`}>
+    <Link
+      to={to}
+      className={`btn btn-${variant} btn-${size} font-${weight} transition duration-300 hover:bg-white`}
+    >
       {children}
     </Link>
   )

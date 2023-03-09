19d829b794e93814bf6745333e88a9dbea6f6b16
Sepehr Gorgini <agent.1378@gmail.com>
2023-03-09 16:37:39 +0330

    Update buttons

--- src/components/ProjectItem.jsx
+++ src/components/ProjectItem.jsx
@@ -1,19 +1,23 @@
 import { useMediaQuery } from '@chakra-ui/react'
 import LinkButton from './LinkButton.jsx'
 
-const ProjectItem = ({ projectName, projectDesc, img }) => {
+const ProjectItem = ({ projectName, projectDesc, slug, img }) => {
   const [isMd] = useMediaQuery('(min-width: 800px)')
 
   return (
-    <div className='flex flex-col items-center mx-auto w-96'>
-      <img src={img} alt='project example' className='mb-6 rounded-xl w-96' />
-      <div>
-        <div className='mb-8'>
-          <h3 className='mb-2 text-2xl font-bold'>{projectName}</h3>
+    <div className='flex flex-col items-center mx-auto transition duration-300 w-96 hover:shadow-xl rounded-2xl'>
+      <img
+        src={img}
+        alt='project example'
+        className='object-cover w-full mb-6 rounded-b-none rounded-xl max-h-60'
+      />
+      <div className='px-4 pb-4'>
+        <div className='mb-6'>
+          <h3 className='mb-3 text-2xl font-bold'>{projectName}</h3>
           <p>{projectDesc}</p>
         </div>
         <LinkButton
-          to='/projects'
+          to={`/projects/${slug}`}
           variant='warning'
           weight={isMd ? 'bold' : 'normal'}
         >

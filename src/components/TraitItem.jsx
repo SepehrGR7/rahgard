ac6fe4cf37041002d5aafead745666634565e64b
Sepehr Gorgini <agent.1378@gmail.com>
2023-03-10 23:14:55 +0330

    Adjust image height

--- src/components/TraitItem.jsx
+++ src/components/TraitItem.jsx
@@ -2,7 +2,7 @@ const TraitItem = ({ image, title, emoji, description }) => {
   return (
     <div className='mx-auto my-5 transition duration-300 shadow-xl border-gray200 md:my-0 card w-80 md:w-72 backdrop-blur-lg '>
       <figure>
-        <img src={image} alt='trait' className='object-cover w-full h-48' />
+        <img src={image} alt='trait' className='object-cover w-full h-44' />
       </figure>
       <div className='card-body'>
         <div className='grid items-center w-12 h-12 mb-3 bg-orange-100 rounded-xl backdrop-blur place-content-center'>

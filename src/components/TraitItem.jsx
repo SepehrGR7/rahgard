e6bf7e187697cc4ce5e655f3f733717aa665d6bc
Sepehr Gorgini <agent.1378@gmail.com>
2023-03-10 23:10:35 +0330

    Change background and spacing

--- src/components/TraitItem.jsx
+++ src/components/TraitItem.jsx
@@ -1,8 +1,8 @@
 const TraitItem = ({ image, title, emoji, description }) => {
   return (
-    <div className='mx-auto transition duration-300 card w-80 md:w-80 glass'>
+    <div className='mx-auto my-5 transition duration-300 shadow-xl border-gray200 md:my-0 card w-80 md:w-72 backdrop-blur-lg '>
       <figure>
-        <img src={image} alt='trait' className='object-cover w-full h-40 lg:h-52' />
+        <img src={image} alt='trait' className='object-cover w-full h-48' />
       </figure>
       <div className='card-body'>
         <div className='grid items-center w-12 h-12 mb-3 bg-orange-100 rounded-xl backdrop-blur place-content-center'>
@@ -11,7 +11,7 @@ const TraitItem = ({ image, title, emoji, description }) => {
         <div className='flex items-center'>
           <h2 className='mb-2 text-xl font-bold'>{title}</h2>
         </div>
-        <p className='font-normal text-justify '>{description}</p>
+        <p className='font-normal leading-7 text-justify'>{description}</p>
       </div>
     </div>
   )

2d96ec1ef3996becbca199e4e01d522dfc6b12a0
Sepehr Gorgini <agent.1378@gmail.com>
2023-03-12 20:39:32 +0330

    Add outer container element

--- src/components/Traits.jsx
+++ src/components/Traits.jsx
@@ -8,23 +8,25 @@ const Traits = () => {
   const traitData = useClient('traits')
 
   return (
-    <section className='px-10 py-20 lg:p-20 trait'>
-      <div className='flex flex-col items-center mb-5'>
-        <h2 className='mb-6 text-3xl font-bold text-center md:mb-8 md:text-4xl'>
-          چرا راه گرد بندر؟
-        </h2>
-        <img src={images.divider} alt='divider' className='w-64 md:w-80' />
-      </div>
-      <div className='grid grid-cols-1 gap-10 py-10 mx-auto md:gap-y-20 gap- md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-4 lg:px-10'>
-        {traitData.map(trait => (
-          <TraitItem
-            key={trait._id}
-            image={urlFor(trait.imageUrl)}
-            title={trait.title}
-            emoji={trait.emoji}
-            description={trait.description}
-          />
-        ))}
+    <section className='trait'>
+      <div className='container px-10 py-20 mx-auto '>
+        <div className='flex flex-col items-center mb-5'>
+          <h2 className='mb-6 text-3xl font-bold text-center md:mb-8 md:text-4xl'>
+            چرا راه گرد بندر؟
+          </h2>
+          <img src={images.divider} alt='divider' className='w-64 md:w-80' />
+        </div>
+        <div className='grid grid-cols-1 gap-10 py-10 mx-auto md:gap-y-20 gap- md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-4 lg:px-10'>
+          {traitData.map(trait => (
+            <TraitItem
+              key={trait._id}
+              image={urlFor(trait.imageUrl)}
+              title={trait.title}
+              emoji={trait.emoji}
+              description={trait.description}
+            />
+          ))}
+        </div>
       </div>
     </section>
   )

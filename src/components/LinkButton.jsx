2a52bdf4b4564fe769ed3aa73054badbb4ac2d99
Sepehr Gorgini <agent.1378@gmail.com>
2023-03-12 20:30:48 +0330

    Add width prop

--- src/components/LinkButton.jsx
+++ src/components/LinkButton.jsx
@@ -1,10 +1,10 @@
 import { Link } from 'react-router-dom'
 
-const LinkButton = ({ to, variant, size, weight, children }) => {
+const LinkButton = ({ to, variant, size, weight, children, width }) => {
   return (
     <Link
       to={to}
-      className={`btn btn-${variant} btn-${size} font-${weight} transition duration-300 hover:bg-white`}
+      className={`btn btn-${variant} btn-${size} font-${weight} transition duration-300 hover:bg-white w-${width} md:w-auto md:font-bold font-normal`}
     >
       {children}
     </Link>
